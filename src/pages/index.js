39c6d74770a737f17d1b1357b36eeae5c149596c
Ricll <ricllima@hotmail.com>
2020-11-16 18:50:56 -0300

    Add Country list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import CountriesTable from '../components/CountriesTable/CountriesTable'
 import Layout from '../components/Layout/Layout'
 import SearchInput from '../components/SearchInput/SearchInput'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ countries }) {
+
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter
+  ((country) => 
+  country.name.toLowerCase().includes(keyword) ||
+  country.region.toLowerCase().includes(keyword) ||
+  country.subregion.toLowerCase().includes(keyword))
+
+  const onInputChange = (e) => {
+    e.preventDefault()
+    setKeyword(e.target.value.toLowerCase())
+  }
   
   return (
     <Layout>
@@ -12,9 +26,12 @@ export default function Home({ countries }) {
         Found {countries.length} countries
       </div>
 
-      <SearchInput/>
+      <SearchInput
+       placeholder="Filter by Name, Region or SubRegion"
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries}/>
+      <CountriesTable countries={filteredCountries}/>
 
     </Layout>
   )
